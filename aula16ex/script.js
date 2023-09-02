25dd41a8dad507880061ebab9d0ed217b545f210
Isabelle Peres da Luz <isabelle_git@hotmail.com>
2023-09-02 15:42:52 -0300

    commit

---
+++ aula16ex/script.js
@@ -0,0 +1,45 @@
+var num = document.querySelector('input#num')
+var lista = document.querySelector('select#lista')
+var res = document.getElementById(res)
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+} 
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Valor invalido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        var tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+    }
+}
